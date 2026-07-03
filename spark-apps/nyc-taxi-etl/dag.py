049764aeadd40bf4a37f4da1c7e8334814b313f2
Kaveh Razavi <kaveh.razavi@gmail.com>
2026-07-03 10:20:54 -0400

    feat(spark-apps): NycTaxiEtl entrypoint + Jenkinsfile + Airflow dag (A5/A6)

--- spark-apps/nyc-taxi-etl/dag.py
+++ spark-apps/nyc-taxi-etl/dag.py
@@ -1,2 +1,24 @@
-"""Airflow DAG for nyc-taxi-etl (implemented in Task 4)."""
+"""Airflow DAG: run the nyc-taxi-etl JAR (published to s3a://jars) on the Spark cluster."""
 from __future__ import annotations
+
+import pendulum
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+
+with DAG(
+    dag_id="nyc_taxi_etl",
+    schedule="@daily",
+    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["data-eng-lab", "spark-app"],
+) as dag:
+    SparkSubmitOperator(
+        task_id="submit_nyc_taxi_etl",
+        conn_id="spark_default",
+        application="s3a://jars/nyc-taxi-etl/0.1.0/app.jar",
+        java_class="com.thekaveh.dataeng.nyctaxi.NycTaxiEtl",
+        conf={
+            "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
+            "spark.hadoop.fs.s3a.path.style.access": "true",
+        },
+    )
